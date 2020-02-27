945cbcbf5348923f755e7ef78c4c9fb0b82ff00a
Amon-Ra Sanders <mrlive2k@gmail.com>
2020-02-27 21:50:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,7 +24,7 @@ def monopoly_with_third_tier
     :pieces=>4,
     :rent_in_dollars=> {
       :one_piece_owned=> 25,
-      :two_piece_owned=> 50,
+      :two_pieces_owned=> 50,
       :third_piece=> 6,
       :fourth_piece=> 7
     },
