ac55269a27df5ef33c0775d60d470495b074c5ca
Amon-Ra Sanders <mrlive2k@gmail.com>
2020-02-27 21:53:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,7 +31,7 @@ def monopoly_with_third_tier
     :names=> {
     :reading_railroad=> {},
     :pennsylvania_railroad=> {},
-    :name_three=> "Sierra",
+    :b_o_railroad=> {},
     :name_four=> "Amon-Ra"
     }
   } 
