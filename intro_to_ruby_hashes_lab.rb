d86a3156127afcfc6ede59f02976e232d030d100
Amon-Ra Sanders <mrlive2k@gmail.com>
2020-02-27 21:40:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,10 +23,10 @@ def monopoly_with_third_tier
   :railroads=> {
     :pieces=>4,
     :rent_in_dollars=> {
-      :elementary
-      :Middleschool
-      :highschool
-      :preschool
+      :elementary,
+      :Middleschool,
+      :highschool,
+      :preschool,
     }
   } 
     
